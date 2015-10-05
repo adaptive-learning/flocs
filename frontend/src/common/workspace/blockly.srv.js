a1c360c320cba4da828eab5beee67941f1b27999
Jaroslav Čechák <jarekcechak@gmail.com>
2015-10-05 22:54:28 +0200

    Definition of new blocks has been added.

--- frontend/src/common/workspace/blockly.srv.js
+++ frontend/src/common/workspace/blockly.srv.js
@@ -22,6 +22,17 @@ angular.module('flocs.workspace')
         "helpUrl": ""
       },
       {
+        "id": "maze_move_backward",
+        "lastDummyAlign0": "LEFT",
+        "message0": "krok vzad",
+        "args0": [],
+        "previousStatement": true,
+        "nextStatement": true,
+        "colour": 120,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
         "id": "maze_turn_left",
         "lastDummyAlign0": "LEFT",
         "message0": "zatoc doleva",
@@ -43,6 +54,83 @@ angular.module('flocs.workspace')
         "tooltip": "",
         "helpUrl": ""
       },
+      {
+        "id": "maze_scan_path_left",
+        "lastDummyAlign0": "LEFT",
+        "message0": "je cesta vlevo",
+        "output": "Boolean",
+        "colour": 210,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "id": "maze_scan_path_right",
+        "lastDummyAlign0": "LEFT",
+        "message0": "je cesta vpravo",
+        "output": "Boolean",
+        "colour": 210,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "id": "maze_if_then",
+        "message0": "pokud %1 %2 pak %3 %4",
+        "args0": [
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_value",
+            "name": "condition",
+            "check": "Boolean"
+        },
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_statement",
+            "name": "condition_true"
+        }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 330,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+      {
+        "id": "maze_if_then_else",
+        "message0": "pokud %1 %2 pak %3 %4 jinak %5 %6",
+        "args0": [
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_value",
+            "name": "condition",
+            "check": "Boolean"
+        },
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_statement",
+            "name": "condition_true"
+        },
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_statement",
+            "name": "condition_false"
+        }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 330,
+        "tooltip": "",
+        "helpUrl": ""
+      }
     ];
     // load all blocks
     for (var i = 0, jsonBlock; jsonBlock = blocksList[i]; i++) {
@@ -58,6 +146,52 @@ angular.module('flocs.workspace')
     };
     Blockly.JavaScript['maze_turn_right'] = function(block) {
       return 'turnRight();';
+    };
+
+    Blockly.JavaScript['maze_move_backward'] = function(block) {
+      return 'moveBackward();';
+    };
+
+    Blockly.JavaScript['maze_scan_path_left'] = function(block) {
+      return 'scanPathLeft();';
+    };
+
+    Blockly.JavaScript['maze_scan_path_right'] = function(block) {
+      return 'scanPathRight();';
+    };
+
+    Blockly.JavaScript['maze_if_then'] = function(block) {
+      // get condition value (True or False)
+      var value_condition = 
+          Blockly.JavaScript.valueToCode(block,
+                  'condition',
+                  Blockly.JavaScript.ORDER_ATOMIC);
+      // transform inner blocks to code
+      var statements_condition_true = 
+          Blockly.JavaScript.statementToCode(block, 'condition_true');
+      // construct Java Script if then statement
+      var code = "if (" + value_condition + ") {"
+          + statements_condition_true + "}";
+      return code;
+    };
+
+    Blockly.JavaScript['maze_if_then_else'] = function(block) {
+        // get condition value (True or False)
+        var value_condition = 
+            Blockly.JavaScript.valueToCode(block,
+                    'condition',
+                    Blockly.JavaScript.ORDER_ATOMIC);
+        // transform inner blocks to code (then branch)
+        var statements_condition_true =
+            Blockly.JavaScript.statementToCode(block, 'condition_true');
+        // transform inner blocks to code (else branch)
+        var statements_condition_false =
+            Blockly.JavaScript.statementToCode(block, 'condition_false');
+        // construct Java Script if then else statement
+        var code = "if (" + value_condition + ") {"
+            + statements_condition_true + "} else {"
+            + statements_condition_false + "}";
+        return code;
     };
     // TODO: code for Python...
   }
