4e42691eed077263eecc5a26ce60ce68ef20d043
Tom Effenberger <tomas.effenberger@gmail.com>
2015-11-15 21:43:45 +0100

    Update task difficulties test

--- practice/models/test_tasks_difficulty_model.py
+++ practice/models/test_tasks_difficulty_model.py
@@ -2,11 +2,10 @@
 """
 
 from django.test import TestCase
-from .tasks_difficulty import TasksDifficultyModel
+from .tasks_difficulty import TasksDifficultyModel, CONCEPT_WEIGHT
 from decimal import Decimal
 
 class TasksDifficultyModelTest(TestCase):
-
     def test_to_vector(self):
         task_difficulties = TasksDifficultyModel(
                 programming=Decimal('-0.58'),
@@ -18,13 +17,13 @@ class TasksDifficultyModelTest(TestCase):
                 pits=False,
                 )
         task_vector = task_difficulties.to_vector()
-        self.assertEquals(Decimal('-0.58'), task_vector[0])
-        self.assertEquals(-1, task_vector[1])
-        self.assertEquals(1, task_vector[2])
-        self.assertEquals(-1, task_vector[3])
-        self.assertEquals(-1, task_vector[4])
-        self.assertEquals(-1, task_vector[5])
-        self.assertEquals(-1, task_vector[6])
+        self.assertAlmostEquals(-0.58, task_vector[0])
+        self.assertEquals(0, task_vector[1])
+        self.assertEquals(CONCEPT_WEIGHT, task_vector[2])
+        self.assertEquals(0, task_vector[3])
+        self.assertEquals(0, task_vector[4])
+        self.assertEquals(0, task_vector[5])
+        self.assertEquals(0, task_vector[6])
 
     def test_number_of_concepts(self):
         task_difficulties = TasksDifficultyModel(
