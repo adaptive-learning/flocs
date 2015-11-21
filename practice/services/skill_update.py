59a48736e92c9a153b4106cfdbf73e9f3f50280f
Jaroslav Čechák <jarekcechak@gmail.com>
2015-11-21 14:54:16 +0100

    Skill update has been fully implemented. Test for skill update has been added. This closes #48.

--- practice/services/skill_update.py
+++ practice/services/skill_update.py
@@ -1,33 +1,166 @@
 """Updates skill of student and difficulty of task
 """
+from math import exp
+from common.flow_factors import FlowFactors
 
-def update_skill(student, task, feedback, predicted_feedback):
+# Constants
+# Speed of prediction learning for student bias
+STUDENT_GLOBAL_SPEED = 1
+
+# Speed of student learing for bias
+STUDENT_GLOBAL_STEP = 1/4
+
+# Steepness of exponential increase for bias
+STUDENT_GLOBAL_STEEPNESS = 1/8
+
+# Speed of task difficulty prediction learning
+TASK_GLOBAL_SPEED = 1
+
+def update_skill(student_id, task_id, reported_flow, predicted_flow,
+                 practice_context):
     """Updates student's skill as well as task's difficulty base on predicted
     and real feedback from the student. Update is based on ELO.
 
     Args:
-        student: vector of student's skills
-        task: vector of task's difficulties
-        feedback: real feedback collected from the student
-        predicted_feedback: feedback predicted by out model of student and task
+        student_id: id of student
+        task_id: id of task the student finnished
+        reported_flow: real flow collected from the student
+        predicted_flow: flow predicted by our model for student and task
+        practice_context: object with skill a difficulty vectors
+    """
+    update_student_skill(
+        student_id,
+        task_id,
+        reported_flow,
+        predicted_flow,
+        practice_context
+    )
+
+    update_task_difficulty(
+        task_id,
+        reported_flow,
+        predicted_flow,
+        practice_context
+    )
+
+def update_student_skill(
+        student_id, task_id, reported_flow, predicted_flow, practice_context):
+    """ Updates student skill.
+
+    Args:
+        student_id: id of student
+        task_id: id of task the student finnished
+        reported_flow: real flow collected from the student
+        predicted_flow: flow predicted by our model for student and task
+        practice_context: object with skill a difficulty vectors
+    """
+    difficulty_dict = practice_context.get_difficulty_dict(task_id)
+
+    for key in FlowFactors.game_factors() + FlowFactors.concept_factors():
+        practice_context.update(
+            key,
+            student=student_id,
+            task=None,
+            update=lambda original: update_other_skill_function(
+                original,
+                reported_flow,
+                difficulty_dict[key]
+            )
+        )
+
+    practice_context.update(
+        FlowFactors.STUDENT_BIAS,
+        student=student_id,
+        task=None,
+        update=lambda original: update_global_skill_function(
+            original,
+            predicted_flow,
+            reported_flow
+        )
+    )
+
+def update_task_difficulty(
+        task_id, reported_flow, predicted_flow, practice_context):
+    """ Updates task difficulty.
+
+    Args:
+        task_id: id of task the student finnished
+        reported_flow: real flow collected from the student
+        predicted_flow: flow predicted by our model for student and task
+        practice_context: object with skill a difficulty vectors
     """
 
-    # coefficients of decay function (the more answers is collected the less
-    # effect will the new answer have)
-    alpha = 1
-    beta = 1
+    solution_count = practice_context.get_solution_count(task_id)
 
-    # same as in ELO for amateur chess players; later might by substituted by
-    # PFA prediction
-    k = 32
+    practice_context.update(
+        FlowFactors.TASK_BIAS,
+        student=None,
+        task=task_id,
+        update=lambda original: update_global_difficulty_function(
+            original,
+            solution_count,
+            predicted_flow,
+            reported_flow
+        )
+    )
 
-    # number of previously collected answers, not implemented yet
-    answers = 1
 
-    # for all skills that the task tests update the students skill
-    for i in range(len(student)):
-        if task[i] != -1:
-            student[i] += k * (predicted_feedback - feedback)
+#def update_skill_function_full(
+#        original, k_1, predicted_flow, reported_flow, a, k_2, k_3):
+#    """ Function for computing new value of student skill.
+#
+#    Args:
+#        original: original value of student skill
+#        k_1: first constant
+#        reported_flow: real flow collected from the student
+#        predicted_flow: flow predicted by our model for student and task
+#        a: concept discrimination
+#        k_2: second constant
+#        k_3: third constant
+#    """
+#
+#    return original - k_1 * (predicted_flow - reported_flow) * a \
+#            + k_2 * exp((-k_3) * original) * a
 
-    # updates general difficulty of task
-    task[0] += alpha / (1 + beta * answers)  * (feedback - predicted_feedback)
+def update_global_skill_function(
+        original, predicted_flow, reported_flow):
+    """ Function for computing new value of student global skill.
+
+    Args:
+        original: original value of student skill
+        k_1: first constant
+        reported_flow: real flow collected from the student
+        predicted_flow: flow predicted by our model for student and task
+        a: concept discrimination
+        k_2: second constant
+        k_3: third constant
+    """
+    return original - STUDENT_GLOBAL_SPEED * (predicted_flow - reported_flow) \
+            + STUDENT_GLOBAL_STEP * exp((-STUDENT_GLOBAL_STEEPNESS) * original)
+
+def update_other_skill_function(original, reported_flow, discrimination):
+    """ Function for computing new value of student game and programming skill.
+
+    Args:
+        original: original value of student skill
+        reported_flow: real flow collected from the student
+    """
+    if discrimination != 1:
+        return original
+    else:
+        return max(original, reported_flow)
+
+
+def update_global_difficulty_function(
+        original, solution_count, predicted_flow, reported_flow):
+    """ Function for compution new value of task difficulty
+
+    Args:
+        original: original value of task difficulty
+        k_4: first constant
+        solution_count: number of successful solutions submitted
+        reported_flow: real flow collected from the student
+        predicted_flow: flow predicted by our model for student and task
+    """
+    return original + TASK_GLOBAL_SPEED/solution_count \
+            * (predicted_flow - reported_flow)
---
+++ practice/services/test_skill_update.py
@@ -0,0 +1,99 @@
+"""Unit test for skill update modul.
+"""
+
+from django.test import TestCase
+from common.flow_factors import FlowFactors
+from practice.models.practice_context import PracticeContext
+import practice.services.skill_update as skill_update
+
+class UpdateSkillTest(TestCase):
+
+    def test_update_global_skill_function(self):
+        skill = 0.25
+        predicted_flow = 0
+        reported_flow = -1
+        expected = -0.50769169
+
+        result = skill_update.update_global_skill_function(
+            skill, predicted_flow, reported_flow)
+
+        self.assertAlmostEquals(result, expected)
+
+    def test_update_other_skill_function(self):
+        discrimination = 0
+
+        for skill in range(-1, 2, 1):
+            for reported_flow in range(-1, 2, 1):
+                result = skill_update.update_other_skill_function(
+                    skill, reported_flow, discrimination)
+                self.assertEquals(result, skill)
+
+        discrimination = 1
+        skill = -1
+        for reported_flow in range(-1, 2, 1):
+            result = skill_update.update_other_skill_function(
+                skill, reported_flow, discrimination)
+            self.assertEquals(result, reported_flow)
+
+        skill = 1
+        for reported_flow in range(-1, 2, 1):
+            result = skill_update.update_other_skill_function(
+                skill, reported_flow, discrimination)
+            self.assertEquals(result, 1)
+
+    def test_update_global_difficulty_function(self):
+        difficulty = 0.25
+        solution_count = 10
+        predicted_flow = 0
+        reported_flow = 1
+        expected = 0.15
+
+        result = skill_update.update_global_difficulty_function(
+            difficulty, solution_count, predicted_flow, reported_flow)
+
+        self.assertAlmostEquals(result, expected)
+
+    def test_update_skill(self):
+        context = PracticeContext([
+            (FlowFactors.STUDENT_BIAS,  1, None, 0.20),
+            (FlowFactors.CONDITIONS,    1, None, 0),
+            (FlowFactors.LOOPS,         1, None, 0),
+            (FlowFactors.LOGIC_EXPR,    1, None, -1),
+            (FlowFactors.COLORS,        1, None, 0),
+            (FlowFactors.TOKENS,        1, None, 1),
+            (FlowFactors.PITS,          1, None, -1),
+            (FlowFactors.TASK_BIAS,     None, 2, 3.7),
+            (FlowFactors.CONDITIONS,    None, 2, 1),
+            (FlowFactors.LOOPS,         None, 2, 0),
+            (FlowFactors.LOGIC_EXPR,    None, 2, 0),
+            (FlowFactors.COLORS,        None, 2, 1),
+            (FlowFactors.TOKENS,        None, 2, 1),
+            (FlowFactors.PITS,          None, 2, 0),
+            ('solution-count',          None, 2, 10)
+        ])
+
+        expected_context = PracticeContext([
+            (FlowFactors.STUDENT_BIAS,  1, None, 1.44382747),
+            (FlowFactors.CONDITIONS,    1, None, 1),
+            (FlowFactors.LOOPS,         1, None, 0),
+            (FlowFactors.LOGIC_EXPR,    1, None, -1),
+            (FlowFactors.COLORS,        1, None, 1),
+            (FlowFactors.TOKENS,        1, None, 1),
+            (FlowFactors.PITS,          1, None, -1),
+            (FlowFactors.TASK_BIAS,     None, 2, 3.6),
+            (FlowFactors.CONDITIONS,    None, 2, 1),
+            (FlowFactors.LOOPS,         None, 2, 0),
+            (FlowFactors.LOGIC_EXPR,    None, 2, 0),
+            (FlowFactors.COLORS,        None, 2, 1),
+            (FlowFactors.TOKENS,        None, 2, 1),
+            (FlowFactors.PITS,          None, 2, 0),
+            ('solution-count',          None, 2, 10)
+        ])
+        skill_update.update_skill(1, 2, 1, 0, context)
+
+        for key in context.get_skill_dict(1):
+            self.assertAlmostEquals(
+                context.get(key, 1, None), expected_context.get(key, 1, None))
+        for key in context.get_difficulty_dict(2):
+            self.assertAlmostEquals(
+                context.get(key, None, 2), expected_context.get(key, None, 2))
