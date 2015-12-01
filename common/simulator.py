21f7aaa9fbecd622f61ddb24ddbf79c318da7e70
Tom Effenberger <tomas.effenberger@gmail.com>
2015-12-01 20:27:12 +0100

    Cleaner simulation output (without tests-related stuff)

--- common/simulator.py
+++ common/simulator.py
@@ -1,5 +1,6 @@
 from django.test.runner import DiscoverRunner
 from datetime import datetime
+import unittest
 
 class Simulator(object):
     """
@@ -30,4 +31,51 @@ class _PassiveTestRunner(DiscoverRunner):
             suite.addTest(test)
         return suite
 
+    def run_suite(self, suite, **kwargs):
+        """
+        """
+        result = self.test_runner(
+            verbosity=self.verbosity,
+            failfast=self.failfast,
+            resultclass=_QuietTestResult,
+            stream=_QuietStream()
+        ).run(suite)
+        print('-' * 70)
+        return result
 
+class _QuietStream(object):
+    """
+    Stream used for simulation runner.
+    """
+    def write(self, text):
+        #text = text.replace('test', 'simulation')
+        #print(text, end='')
+        pass
+
+    def flush(self):
+        pass
+
+
+class _QuietTestResult(unittest.TextTestResult):
+    """
+    Test result class which is quit most of the time.
+    Used for simulations, we don't want to show dots etc.
+    """
+
+    def addSuccess(self, test):
+        pass
+
+    def addError(self, test, err):
+        pass
+
+    def addFailure(self, test, err):
+        pass
+
+    def addSkip(self, test, reason):
+        pass
+
+    def addExpectedFailure(self, test, err):
+        pass
+
+    def addUnexpectedSuccess(self, test):
+        pass
