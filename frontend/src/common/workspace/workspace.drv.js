37db1b55e1ec1507a24d42c6655683c2945314b0
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2015-12-31 17:22:30 +0100

    Blockly filling available space
    Resolves #7
    More styling needed, especially for small screens.

--- frontend/src/common/workspace/workspace.drv.js
+++ frontend/src/common/workspace/workspace.drv.js
@@ -2,7 +2,7 @@
  * Workspace Directive
  */
 angular.module('flocs.workspace')
-.directive('flocsWorkspace', ['workspaceService', function(workspaceService) {
+.directive('flocsWorkspace', ['$window', 'workspaceService', function($window, workspaceService) {
   return {
     restrict: 'E',
     scope: {},
@@ -11,14 +11,44 @@ angular.module('flocs.workspace')
     },*/
     link: function(scope, element, attrs) {
 
-      // inject blockly into the workspace
-      var blocklyDiv = Blockly.inject('blocklyDiv', {
-        toolbox: '<xml></xml>',
-        trashcan: true
-      });
+      var blocklyArea = document.getElementById('blocklyArea');
+      var blocklyDiv = document.getElementById('blocklyDiv');
 
-      workspaceService.setBlocklyDiv(blocklyDiv);
+      function resize(e) {
+        // The official manual for blockly resizing is over-complicated, see:
+        // https://developers.google.com/blockly/installation/injecting-resizable
+        blocklyDiv.style.left = '0px';
+        blocklyDiv.style.top = '0px';
+        blocklyDiv.style.width = '100%';
+        blocklyDiv.style.height = '100%';
+      }
+      angular.element($window).bind('resize', resize);
+      resize();
+      //workspaceService.setBlocklyDiv(blocklyDiv);
     }
   };
 }]);
 
+
+/*angular.module('flocs.workspace')
+.directive('fillSpace', ['$window', function($window) {
+  return function(scope, element, attrs) {
+
+    function resize() {
+      //var w = angular.element($window);
+      var innerHeight = $window.innerHeight;
+      var innerWidth = $window.innerWidth;
+      console.log('window.inner..', innerHeight, innerWidth);
+      console.log('parent..', element.prop('offsetWidth'));
+      var width = 100;
+      var height = 100;
+      element.css('width',  width + 'px');
+      element.css('height', height + 'px');
+      console.log('resize to', width, height);
+    }
+
+    angular.element($window).bind('resize', resize);
+    resize();
+
+  };
+}]);*/
