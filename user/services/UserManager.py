cff04d2170c2ae5ab3ac9c5959126cd728bdb134
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-01-24 11:56:15 +0100

    Fix logging after signing up of lazy users

--- user/services/UserManager.py
+++ user/services/UserManager.py
@@ -7,24 +7,33 @@ from django.contrib.auth import authenticate, login as log, logout as djangologo
 def register(request, username, firstname, lastname, email, passwd):
     user = request.user
     if is_lazy_user(user):
-        user.username = username
-        user.email = email
-        user.set_password(passwd)
-        user.save()
-        LazyUser.objects.filter(user=user).delete()
-        converted.send(None, user=user)
+        convert_lazy_user(user, username, email, passwd)
     elif user.is_anonymous():
         user = User.objects.create_user(username, email=email, password=passwd,
                 first_name=firstname, last_name=lastname)
     login(request, username, passwd)
 
 
+def convert_lazy_user(user, username, email, password):
+    # TODO: proper error handling of user with the used username (or email,
+    # depending what we will use for identification)
+    user.username = username
+    user.email = email
+    user.set_password(password)
+    user.backend = None
+    user.save()
+    LazyUser.objects.filter(user=user).delete()
+    converted.send(None, user=user)
+    assert is_lazy_user(user) == False
+
+
 def login(request, username, password):
-    if is_lazy_user(request.user):
-        LazyUser.objects.filter(user=request.user).delete()
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
+            if is_lazy_user(request.user):
+                LazyUser.objects.filter(user=request.user).delete()
+                request.user.delete()
             log(request, user)
             return True
         else:
