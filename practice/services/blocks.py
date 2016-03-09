66cfab6fb8796a460a915aa5f03426abf397abe3
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-03-09 15:44:49 +0100

    Generator for all next blocks, handling no next block

--- practice/services/blocks.py
+++ practice/services/blocks.py
@@ -1,16 +1,39 @@
-from itertools import chain
+from itertools import *
 from blocks.models import BlockModel
 from practice.models import StudentModel
 
 
 def get_next_block_for_student(student):
     """
-    Returns next block (in order given by blocks' difficulties) which is not
-    yet owned by the student
+    Returns next block which is not yet owned by the student.
+    Order is given by difficulties of blocks.
+
+    Raises NoNextBlock if the student already owns all blocks.
     """
-    all_ordered_blocks = BlockModel.objects.all_ordered()
-    students_ordered_blocks = student.available_blocks.all_ordered()
-    for possible_block, students_block in zip(all_ordered_blocks,
-                                              chain(students_ordered_blocks, [None])):
-        if possible_block != students_block:
-            return possible_block
+    try:
+        return next(next_blocks_for_student(student))
+    except StopIteration:
+        raise NoNextBlock('Student {0} already owns all blocks.'.format(student.pk))
+
+
+def next_blocks_for_student(student):
+    """
+    Returns generator of blocks not yet owned by given student.
+    Order is given by difficulties of blocks.
+    """
+    all_blocks_ordered = BlockModel.objects.all_ordered()
+    owned_blocks_ordered = chain(student.available_blocks.all_ordered(),
+                                 repeat(None))
+    cheapest_owned_block = next(owned_blocks_ordered)
+    for block in all_blocks_ordered:
+        if block == cheapest_owned_block:
+            cheapest_owned_block = next(owned_blocks_ordered)
+        else:
+            yield block
+
+
+class NoNextBlock(LookupError):
+    """
+    Exception raised when next block is requested, but there is no next block.
+    """
+    pass
--- practice/services/test_blocks.py
+++ practice/services/test_blocks.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from blocks.models import BlockModel
 from practice.models import StudentModel
+from practice.services.blocks import next_blocks_for_student, NoNextBlock
 from practice.services.blocks import get_next_block_for_student
 
 
@@ -14,13 +15,22 @@ class BlocksServiceTest(TestCase):
         self.block2 = BlockModel.objects.create(name='b2', difficulty=0.2)
         self.block3 = BlockModel.objects.create(name='b3', difficulty=0.3)
         self.block4 = BlockModel.objects.create(name='b4', difficulty=0.4)
-        self.student.available_blocks = [self.block1, self.block2]
+        self.student.available_blocks = [self.block1, self.block3]
         self.student.save()
+
+    def test_next_blocks_for_student(self):
+        next_blocks = list(next_blocks_for_student(self.student))
+        self.assertEquals(next_blocks, [self.block2, self.block4])
+
+    def test_no_next_blocks_for_student(self):
+        self.student.available_blocks = [self.block1, self.block2, self.block3, self.block4]
+        self.student.save()
+        next_blocks = list(next_blocks_for_student(self.student))
+        self.assertEquals(next_blocks, [])
 
     def test_get_next_block_for_student(self):
         next_block = get_next_block_for_student(self.student)
-        self.assertEqual(next_block, self.block3)
-
+        self.assertEqual(next_block, self.block2)
 
     def test_get_two_next_blocks_for_student(self):
         next_block = get_next_block_for_student(self.student)
@@ -28,3 +38,9 @@ class BlocksServiceTest(TestCase):
         self.student.save()
         next_block2 = get_next_block_for_student(self.student)
         self.assertEqual(next_block2, self.block4)
+
+    def test_get_next_block_for_student_all(self):
+        self.student.available_blocks = [self.block1, self.block2, self.block3, self.block4]
+        self.student.save()
+        with self.assertRaises(NoNextBlock):
+            get_next_block_for_student(self.student)
