6441cc05aa3a7b12031435c4e2ecd003c446e73d
Jiří Mauritz <jirmauritz@gmail.com>
2016-03-30 15:31:20 +0200

    SessionOverview: fixed links and hidden hours
    Resolves #149

--- frontend/src/common/filters/secondsToTime.js
+++ frontend/src/common/filters/secondsToTime.js
@@ -6,6 +6,7 @@
 angular.module('flocs.filters')
 .filter('secondsToTime', function($filter) {
     return function(seconds) {
-        return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), 'HH:mm:ss');
+        var format = (seconds >= 3600) ? 'HH:mm:ss' : 'mm:ss';
+        return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), format);
     };
 });
