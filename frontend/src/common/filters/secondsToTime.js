0ffacbbce1b9924c07e695671019a7389fc39543
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-05-20 14:33:07 +0200

    Format time in solved tasks table

--- frontend/src/common/filters/secondsToTime.js
+++ frontend/src/common/filters/secondsToTime.js
@@ -6,7 +6,7 @@
 angular.module('flocs.filters')
 .filter('secondsToTime', function($filter) {
     return function(seconds) {
-        var format = (seconds >= 3600) ? 'HH:mm:ss' : 'mm:ss';
+        var format = (seconds >= 3600) ? 'H:mm:ss' : "m:ss";
         return $filter('date')(new Date(0, 0, 0).setSeconds(seconds), format);
     };
 });
