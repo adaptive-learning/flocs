7c0168996cef8cf033001a738d5cb788d6ff2ff0
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-05-22 12:34:42 +0200

    Simple time-meter to show time+percentil compactly

--- frontend/src/common/directives/directives.mdl.js
+++ frontend/src/common/directives/directives.mdl.js
@@ -4,6 +4,25 @@
 angular.module('flocs.directives', [
 ])
 
+.directive('flocsTimeMeter', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      'time': '@',
+      'percentil': '@',
+    },
+    template: '<span class="timeMeter" ng-style="percentilStyle" title="percentil {{percentil}} %">{{time | secondsToTime}}</span>',
+    link: function(scope, element, attrs) {
+      // the higher percentil, the more dark yellow color
+      // (shades of yellow are determined by the amount of blue)
+      var blue = Math.round(220 - 2.1 * scope.percentil);
+      scope.percentilStyle = {
+        'background-color': 'rgb(250, 250, ' + blue + ')',
+      };
+    }
+  };
+})
+
 .directive('switchDomain', function($rootScope, $location) {
 
   function appendCurrentPath(domain) {
